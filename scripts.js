a637407d039ce39206291014f7bb70bf27b62e13
rgmccloskey8A5 <rgmccloskey8@gmail.com>
2025-12-05 10:27:29 -0500

    added many more images

--- scripts.js
+++ scripts.js
@@ -1,10 +1,23 @@
 document.getElementById("clickBtn").addEventListener("click", function () {
   const messages = [
     "Snoopy does a happy dance!",
+    "<img src='dancingSnoopy.png' class='msg-img'>",
     "Charlie Brown sighs...",
+    "<img src='charlieBrown.png' class='msg-img'>",
     "Linus drops his blanket in surprise!",
+    "<img src='linus.png' class='msg-img'>",
     "Lucy charges 5 cents for advice.",
-    "Woodstock chirps excitedly!"
+    "<img src='lucy.png' class='msg-img'>",
+    "Woodstock chirps excitedly!",
+    "<img src='woodstock.png' class='msg-img'>"
   ];
-  document.getElementById("message").textContent = messages[Math.floor(Math.random() * messages.length)];
+
+  document.getElementById("message").innerHTML =
+    messages[Math.floor(Math.random() * messages.length)];
+});
+
+
+document.getElementById("danceBtn").addEventListener("click", () => {
+  const snoopy = document.getElementById("snoopyDance");
+  snoopy.style.left = snoopy.style.left === "0px" ? "300px" : "0px";
 });
